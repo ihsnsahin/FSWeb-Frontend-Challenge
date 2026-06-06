e01e5247fd9e0105be38086d64a87442eeb1112a
ihsnsahin <ihsansahin777@gmail.com>
2026-06-06 11:55:34 +0300

    Light Mode tasarım oluşturuldu

--- minimalist-portfolio/src/components/Footer.jsx
+++ minimalist-portfolio/src/components/Footer.jsx
@@ -1,8 +1,28 @@
 function Footer() {
     return (
-        <>
-            <h1>Footer Bölümü</h1>
-        </>
+        <div className="bg-[#F9F9F9] mt-33">
+            <div className="flex flex-col max-w-6xl mx-auto gap-22 pt-21 pb-32">
+                <h2 className="text-[42px] font-semibold text-[#1F2937] max-w-lg "> Let’s work together on your next product.</h2>
+                <div className="flex justify-between">
+                    <div className="text-xl font-medium">
+                        <span> 👉 </span>
+                        <a href="mailto:almilasucode@gmail.com" className=" text-[#AF0C48] underline">
+                            almilasucode@gmail.com
+                        </a></div>
+
+                    <nav>
+                        <ul className="flex gap-6 text-lg font-medium">
+                            <li><a href="#" target="_blank" rel="noreferrer" className="text-[#0A0A14]">Personal Blog</a></li>
+                            <li><a href="#" target="_blank" rel="noreferrer" className="text-[#00AB6B]">Github</a></li>
+                            <li><a href="#" target="_blank" rel="noreferrer" className="text-[#0077B5]">Linkedin</a></li>
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 
--- minimalist-portfolio/src/components/Header.jsx
+++ minimalist-portfolio/src/components/Header.jsx
@@ -1,6 +1,25 @@
 function Header() {
     return (
-        <>
+        <div className="flex flex-col gap-2 pt-5">
+            <div className="flex justify-end items-center gap-4 text-sm font-bold tracking-wider text-indigo-900">
+
+                {/* SOL TARAF: Dark Mode Toggle (Input + Label Yapısı) */}
+                <div className="flex items-center gap-3">
+                    <label className="relative inline-flex items-center cursor-pointer">
+                        <input type="checkbox" className="sr-only peer" />
+                        {/* Switch'in Gövdesi */}
+                        <div className="w-12 h-5 bg-[#4731D3] rounded-full peer peer-focus:outline-none transition-colors duration-300"></div>
+                        {/* Switch'in İçindeki Sarı Top */}
+                        <div className="absolute top-0.5 left-0.5 w-4 h-4 bg-[#FFE86E] rounded-full transition-all duration-300"></div>
+                    </label>
+                    <span className="text-[#777777] text-sm font-bold">DARK MODE</span>
+                </div>
+                <span className="text-[#777777] text-sm font-bold">|</span>
+                {/* SAĞ TARAF: Dil Değiştirme Butonu */}
+                <button className="text-[#777777] text-sm font-bold">
+                    <span className="text-[#4731D3]">TÜRKÇE</span>'YE GEÇ
+                </button>
+            </div>
             <div className="flex justify-between">
                 <span className="w-14 h-14 text-logo-600 bg-[#EEEBFF] rounded-full flex items-center justify-center font-bold text-2xl rotate-40">A</span>
                 <nav className="flex items-center">
@@ -9,7 +28,7 @@ function Header() {
                     <a href="" className="button-header">Hire Me</a>
                 </nav>
             </div>
-        </>
+        </div>
     )
 }
 
