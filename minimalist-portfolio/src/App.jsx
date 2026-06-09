4661cba111b08016c1443916b98fdc07ee8e8485
ihsnsahin <ihsansahin777@gmail.com>
2026-06-09 19:51:05 +0300

    Section bölümü oluşturuldu

--- minimalist-portfolio/src/App.jsx
+++ minimalist-portfolio/src/App.jsx
@@ -1,11 +1,9 @@
 import { Bounce, ToastContainer } from 'react-toastify';
 import './App.css';
 import Footer from './components/Footer';
-import Profile from './components/Profile';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
 import { useTheme } from './contexts/ThemeContext';
 import Main from './components/Main';
+import Section from './components/Section';
 
 
 
@@ -16,9 +14,7 @@ function App() {
       <div className="dark:bg-[#252128] transition-colors duration-300">
         <div className="flex flex-col max-w-6xl  mx-auto gap-22 max-phone:max-w-full max-phone:px-6 max-desktop:px-6">
           <Main />
-          <Skills />
-          <Profile />
-          <Projects />
+          <Section />
         </div>
       </div>
       <Footer />
---
+++ minimalist-portfolio/src/components/Section.jsx
@@ -0,0 +1,14 @@
+import Profile from "./Profile";
+import Projects from "./Projects";
+import Skills from "./Skills";
+
+function Section() {
+
+    return (
+        <>
+            <Skills />
+            <Profile />
+            <Projects />
+        </>);
+}
+export default Section;
